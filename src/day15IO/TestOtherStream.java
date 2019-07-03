02accd3bdfc4e1890dfa0ffa2a3feb9f5cb43d71
3q34 <08chendongxiao@sina.com>
2019-07-03 17:05:11 +0800

    标准输入输出流

--- src/day15IO/TestOtherStream.java
+++ src/day15IO/TestOtherStream.java
@@ -9,9 +9,45 @@ import java.io.*;
  * desc:转换流：InputStreamReader OutputStreamWriter
  * 编码：字符串-->字符数组
  * 解码：字符数组-->字符串
+ * <p>
+ * 标准的输入输出流的使用：System.out System.in 属于printstream
  */
 public class TestOtherStream {
     private static final String TAG = "TestOtherStream";
+
+    public static void main(String[] args) {
+        test4();
+    }
+
+    //标准的输入输出流的使用：System.out System.in
+    //@Test//无法在Junit Test中运行，至少Idea是这样，但是可以在
+    public static void test4() {
+        BufferedReader br = null;
+        try {
+            InputStream inputStream = System.in;
+            InputStreamReader isr = new InputStreamReader(inputStream);
+            br = new BufferedReader(isr);
+            String st;
+            while (true) {
+                System.out.println(" 请输入字符串：");
+                st = br.readLine();
+                if (st.equalsIgnoreCase("e") || st.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                String str = st.toUpperCase();
+                System.out.println(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Test
     public void test() {
